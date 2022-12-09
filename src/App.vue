749cd564eafa6cc3e612b868e8593a60bac59d16
camila <112856775+Camilamellizo@users.noreply.github.com>
2022-12-09 23:48:28 +0100

    weather

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container h-screen">
+  <div class="container ">
     <div class="col-span-12 text-white md:grid grid-cols-12">  
-      <div class="col-span-12 md:col-span-4 ">
+      <div class="col-span-12 bg-blue-sky md:col-span-4 h-screen">
         <Sidebar />
       </div>
       <div class="col-span-12 md:col-span-8">
--- src/components/sideBar.vue
+++ src/components/sideBar.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="bg-blue-sky  bg-fondo bg-center bg-no-repeat px-2 lg:px-8 ">
+    <div class="bg-fondo bg-center bg-no-repeat px-2 lg:px-8 ">
 
-      <div class="pt-4 md:pt-16">
+      <div class="pt-4">
         <div class="flex justify-between">          
           <div>
               <button type="button" class=" bg-gray py-2 px-4">
-                  Seach for places
+                  Search for places
               </button>    
           </div>
 
@@ -15,62 +15,75 @@
               </span>
           </div>
         </div>
+      </div>
 
-        <div>
-          <div class="mt-2"> 
-            <div class="flex justify-end">
-              <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+      <div>
+
+        <div class="mt-2">
+          <!-- x -->
+          <div class="flex justify-end my-2">
+            <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </span>
+          </div>
+        
+          <!-- search + button -->
+          <div class="flex justify-between gap-1 mt-4">
+            <label class="relative block">
+              <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg class="h-5 w-5 fill-gray" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30"
+                  viewBox="0 0 30 30">
+                  <path
+                    d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z">
+                  </path>
                 </svg>
               </span>
+              <input class=" bg-transparent placeholder:font-italitc border border-gray  py-2 pl-10 required"
+                placeholder="Search location" type="text" />
+            </label>
+            <div>
+              <button type="button" class=" bg-blue-buttom py-2 px-2">
+                Search
+              </button>
             </div>
-              <div class="flex flex justify-between gap-1 mt-4">
-                  <label class="relative block">
-                      <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                          <svg class="h-5 w-5 fill-gray" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30"
-                              height="30" viewBox="0 0 30 30">
-                              <path
-                                  d="M 13 3 C 7.4889971 3 3 7.4889971 3 13 C 3 18.511003 7.4889971 23 13 23 C 15.396508 23 17.597385 22.148986 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148986 17.597385 23 15.396508 23 13 C 23 7.4889971 18.511003 3 13 3 z M 13 5 C 17.430123 5 21 8.5698774 21 13 C 21 17.430123 17.430123 21 13 21 C 8.5698774 21 5 17.430123 5 13 C 5 8.5698774 8.5698774 5 13 5 z">
-                              </path>
-                          </svg>
-                      </span>
-                      <input class=" bg-transparent placeholder:font-italitc border border-gray  py-2 pl-10 required"
-                          placeholder="Search location" type="text" />
-                  </label>
-                  <div>
-                      <button type="button" class=" bg-blue-buttom py-2 px-2">
-                          Search
-                      </button>
-                  </div>
-              </div>
-          
-              <div>
-                  
-              </div>
           </div>
         </div>
-      </div>
-
-      <div class="text-6xl grid justify-items-center ">
-        <div>
-          <span>15</span>
-        </div>
-        <div>
-          <span>15</span>
-        </div>
-        <div>
-          <span>15</span>
-        </div>
-        <div>
-          <span>15</span>
-        </div>
-        <div>
-          <span>15</span>
-        </div>
         
+        <div  class="space-y-12">
+          <div>
+            <button class="hover:border hover:border-gray py-2 px-4 w-full flex justify-between">
+              <span>London</span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray"
+                class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+              </svg>  
+            </button>
+          </div>
+          
+          <div>
+            <button class="hover:border hover:border-gray  py-2 px-4 w-full rounded flex justify-between">
+              <span>Barcelona</span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray"
+                class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+              </svg>  
+            </button>
+          </div>
+
+          <div>
+            <button class="hover:border hover:border-gray  py-2 px-4 w-full rounded flex justify-between">
+              <span>Long Beach</span>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="gray"
+                class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+              </svg>  
+            </button>
+          </div> 
+      </div>    
 
       </div>
-
 
     </div>
    
